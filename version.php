5d9571f219720df6b823f6724d40d4a681f8f2fc
Jonathan Garcia <Jonathan.Garcia@openlms.net>
2022-04-13 11:05:22 -0500

    INT-17961: 3.11mp2 version bump

--- version.php
+++ version.php
@@ -9,7 +9,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 /** @var object $plugin */
-$plugin->version   = 2021060100;
+$plugin->version   = 2022041300;
 $plugin->requires  = 2020110900;
 $plugin->release   = '3.10.3';
 $plugin->component = 'availability_releasecode';
