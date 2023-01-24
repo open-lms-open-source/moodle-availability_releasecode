51376df9e66de432a2cd182f38e616ae9c8b7a7f
Julian Tovar <julings12@gmail.com>
2023-01-24 10:58:39 -0500

    INT-18643: 4.0 MP1 Version bump

--- version.php
+++ version.php
@@ -9,7 +9,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 /** @var object $plugin */
-$plugin->version   = 2022122100;
+$plugin->version   = 2023012400;
 $plugin->requires  = 2022041900;
 $plugin->release   = '4.0.5';
 $plugin->component = 'availability_releasecode';
