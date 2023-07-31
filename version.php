966539f1417c86626dc418f1a36819479395ed4e
juanfelipe.orozcoescobar <juanfelipe.orozcoescobar@openlms.net>
2023-07-31 14:20:56 -0500

    INT-19070: 4.1 MP1 Version bump

--- version.php
+++ version.php
@@ -9,7 +9,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 /** @var object $plugin */
-$plugin->version   = 2023061500;
+$plugin->version   = 2023073100;
 $plugin->requires  = 2022112800;
 $plugin->release   = '4.1.3';
 $plugin->component = 'availability_releasecode';
