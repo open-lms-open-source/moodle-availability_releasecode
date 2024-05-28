e31b909089b39fea6b8564912764030d6bd16918
Felipe Vargas <felipe.vargas@openlms.net>
2024-05-28 13:21:44 +0000

    INT-19523: Importing nl lang

---
+++ lang/nl/availability_releasecode.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2024 Open LMS (https://www.openlms.net)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Beperking op uitgiftecode';
+$string['requiresreleasecode'] = 'Je krijgt een bepaalde uitgiftecode.';
+$string['title'] = 'Uitgiftecode';
+$string['description'] = 'Dit cursusitem is niet beschikbaar voor studenten tenzij de student de uitgiftecode verkrijgt die hier is ingevoerd.';
+$string['error_setvalue'] = 'Moet een uitgiftecode invoeren';
+$string['cachedef_releasecodes'] = 'Uitgiftecodes gebruiker';
+$string['privacy:reason'] = 'Er worden geen persoonlijke gegevens opgeslagen bij beperking op uitgiftecode.';
+$string['privacy:metadata:availability_releasecode'] = 'Uitgiftecodes-tabel';
+$string['privacy:metadata:availability_releasecode:code'] = 'Code';
+$string['privacy:metadata:availability_releasecode:courseid'] = 'Cursus';
+$string['privacy:metadata:availability_releasecode:userid'] = 'Gebruiker';
