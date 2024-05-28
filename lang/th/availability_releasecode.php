15494fed1159422f750928d0988dbe9100020273
Felipe Vargas <felipe.vargas@openlms.net>
2024-05-28 13:21:15 +0000

    INT-19523: Importing th lang

---
+++ lang/th/availability_releasecode.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2024 Open LMS (https://www.openlms.net)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'ข้อจำกัดตามรหัสรีลีส';
+$string['requiresreleasecode'] = 'คุณจะได้รับรหัสรีลีสเฉพาะ';
+$string['title'] = 'รหัสรีลีส';
+$string['description'] = 'รายการรายวิชานี้จะไม่สามารถใช้ได้กับผู้เรียนจนกว่าผู้เรียนจะได้รับรหัสรีลีสเฉพาะ';
+$string['error_setvalue'] = 'ต้องป้อนรหัสรีลีส';
+$string['cachedef_releasecodes'] = 'รหัสรีลีสของผู้ใช้งาน';
+$string['privacy:reason'] = 'ข้อจำกัดตามรหัสรีลีสจะไม่จัดเก็บข้อมูลส่วนบุคคลใดๆ';
+$string['privacy:metadata:availability_releasecode'] = 'ตารางรหัสรีลีส';
+$string['privacy:metadata:availability_releasecode:code'] = 'รหัส';
+$string['privacy:metadata:availability_releasecode:courseid'] = 'รายวิชา';
+$string['privacy:metadata:availability_releasecode:userid'] = 'ผู้ใช้งาน';
