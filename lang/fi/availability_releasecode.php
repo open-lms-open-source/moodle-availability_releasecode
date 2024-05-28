238c1467b7140c9396c0d52ac2f6e07b030faa22
Felipe Vargas <felipe.vargas@openlms.net>
2024-05-28 13:22:04 +0000

    INT-19523: Importing fi lang

---
+++ lang/fi/availability_releasecode.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2024 Open LMS (https://www.openlms.net)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Rajoittaminen vapautuskoodilla';
+$string['requiresreleasecode'] = 'Saat erityisen vapautuskoodin.';
+$string['title'] = 'Vapautuskoodi';
+$string['description'] = 'Opiskelijat eivät voi käyttää tätä kurssikohdetta ilman määritettyä vapautuskoodia.';
+$string['error_setvalue'] = 'Vapautuskoodi on annettava';
+$string['cachedef_releasecodes'] = 'Käyttäjien vapautuskoodit';
+$string['privacy:reason'] = 'Rajoittaminen vapautuskoodilla ei tallenna mitään henkilökohtaisia tietoja.';
+$string['privacy:metadata:availability_releasecode'] = 'Vapautuskooditaulukko';
+$string['privacy:metadata:availability_releasecode:code'] = 'Koodi';
+$string['privacy:metadata:availability_releasecode:courseid'] = 'Kurssi';
+$string['privacy:metadata:availability_releasecode:userid'] = 'Käyttäjä';
