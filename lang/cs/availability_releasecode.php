daf2c96c86b24e69fb9744467fc50f8db50bc502
Felipe Vargas <felipe.vargas@openlms.net>
2024-05-28 13:21:59 +0000

    INT-19523: Importing cs lang

---
+++ lang/cs/availability_releasecode.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2024 Open LMS (https://www.openlms.net)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Omezeno podle kódu pro uvolnění';
+$string['requiresreleasecode'] = 'Získáte určitý kód pro uvolnění.';
+$string['title'] = 'Kód pro uvolnění';
+$string['description'] = 'Tato položka kurzu nebude studentům k dispozici, dokud nezískají určitý kód pro uvolnění.';
+$string['error_setvalue'] = 'Musíte zadat kód pro uvolnění';
+$string['cachedef_releasecodes'] = 'Kódy pro uvolnění uživatele';
+$string['privacy:reason'] = 'Omezení podle kódu pro uvolnění neukládá žádné osobní údaje.';
+$string['privacy:metadata:availability_releasecode'] = 'Tabulka kódů pro uvolnění';
+$string['privacy:metadata:availability_releasecode:code'] = 'Kód';
+$string['privacy:metadata:availability_releasecode:courseid'] = 'Kurz';
+$string['privacy:metadata:availability_releasecode:userid'] = 'Uživatel';
