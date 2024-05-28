a4fae23f2eb6f467e47bc9c50f3dc7f780c21ecd
Felipe Vargas <felipe.vargas@openlms.net>
2024-05-28 13:21:18 +0000

    INT-19523: Importing tr lang

---
+++ lang/tr/availability_releasecode.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2024 Open LMS (https://www.openlms.net)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Sürüm koduna göre kısıtlama';
+$string['requiresreleasecode'] = 'Belirli bir sürüm kodu alırsınız.';
+$string['title'] = 'Sürüm kodu';
+$string['description'] = 'Bu kurs öğesi, öğrenci belirli bir sürüm kodu elde edene kadar öğrenciler tarafından kullanılamaz.';
+$string['error_setvalue'] = 'Sürüm kodu girilmeli';
+$string['cachedef_releasecodes'] = 'Kullanıcı sürüm kodları';
+$string['privacy:reason'] = 'Sürüm koduna göre kısıtlama eklentisi, kişisel verileri depolamaz.';
+$string['privacy:metadata:availability_releasecode'] = 'Sürüm kodu tablosu';
+$string['privacy:metadata:availability_releasecode:code'] = 'Kod';
+$string['privacy:metadata:availability_releasecode:courseid'] = 'Kurs';
+$string['privacy:metadata:availability_releasecode:userid'] = 'Kullanıcı';
