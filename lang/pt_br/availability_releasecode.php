50ce73f828671cd230a3d3f104b0138d1e6e18fe
Felipe Vargas <felipe.vargas@openlms.net>
2024-05-28 13:22:21 +0000

    INT-19523: Importing pt_br lang

---
+++ lang/pt_br/availability_releasecode.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2024 Open LMS (https://www.openlms.net)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Restrição por código de liberação';
+$string['requiresreleasecode'] = 'Você recebe um código de liberação em particular.';
+$string['title'] = 'Código de liberação';
+$string['description'] = 'Este item do curso não ficará disponível para os alunos até que eles adquiram um código de liberação específico.';
+$string['error_setvalue'] = 'É necessário inserir um código de liberação';
+$string['cachedef_releasecodes'] = 'Códigos de liberação do usuário';
+$string['privacy:reason'] = 'A Restrição por código de liberação não armazena dados pessoais.';
+$string['privacy:metadata:availability_releasecode'] = 'Tabela de códigos de liberação';
+$string['privacy:metadata:availability_releasecode:code'] = 'Código';
+$string['privacy:metadata:availability_releasecode:courseid'] = 'Curso';
+$string['privacy:metadata:availability_releasecode:userid'] = 'Usuário';
