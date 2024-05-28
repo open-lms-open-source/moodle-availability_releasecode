d8d9cc865da4116c9e63d2a9b004b6f0cc589b76
Felipe Vargas <felipe.vargas@openlms.net>
2024-05-28 13:21:27 +0000

    INT-19523: Importing de lang

---
+++ lang/de/availability_releasecode.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2024 Open LMS (https://www.openlms.net)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Beschränkung durch Freigabecode';
+$string['requiresreleasecode'] = 'Sie erhalten einen speziellen Freigabecode.';
+$string['title'] = 'Freigabecode';
+$string['description'] = 'Dieses Kurselement steht Teilnehmern erst zur Verfügung, nachdem sie einen speziellen Freigabecode erworben haben.';
+$string['error_setvalue'] = 'Eine Freigabecode muss eingegeben werden';
+$string['cachedef_releasecodes'] = 'Nutzer/innen-Freigabecodes';
+$string['privacy:reason'] = 'Das Plugin "Beschränkung nach Freigabecode" speichert keine personenbezogenen Daten.';
+$string['privacy:metadata:availability_releasecode'] = 'Tabelle mit Freigabecodes';
+$string['privacy:metadata:availability_releasecode:code'] = 'Code';
+$string['privacy:metadata:availability_releasecode:courseid'] = 'Kurs';
+$string['privacy:metadata:availability_releasecode:userid'] = 'Nutzer/in';
