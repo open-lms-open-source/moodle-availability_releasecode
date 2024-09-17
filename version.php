cf39c8f896412904f395fa3cce1f735310ea4a4b
Rafael Monterroza <Rafael.Monterroza@openlms.net>
2024-09-17 07:39:16 -0500

    INT-20209: 4.3MP2 Version Bump of Open LMS plugins

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 /** @var object $plugin */
-$plugin->version   = 2024072700;
+$plugin->version   = 2024091700;
 $plugin->requires  = 2023100900;
 $plugin->release   = '4.3.4';
 $plugin->component = 'availability_releasecode';
